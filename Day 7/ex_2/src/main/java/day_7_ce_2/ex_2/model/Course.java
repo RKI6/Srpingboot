ff0e6aeec380d25c5721a05555939a64f6304561
RKI6 <124854958+RKI6@users.noreply.github.com>
2024-02-29 11:22:14 +0530

    Add files via upload

---
+++ Day 7/ex_2/src/main/java/day_7_ce_2/ex_2/model/Course.java
@@ -0,0 +1,9 @@
+package day_7_ce_2.ex_2.model;
+
+public class Course {
+    private int courseId;
+    private String courseName;
+    private String instructor;
+    private int creditHours ;
+    private String preRequisites;
+}
---
+++ Day 7/ex_2/src/test/java/day_7_ce_2/ex_2/Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package day_7_ce_2.ex_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
