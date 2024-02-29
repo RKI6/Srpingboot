43cb495430efd27b4fb9533bd8cc78f02b3c804c
RKI6 <124854958+RKI6@users.noreply.github.com>
2024-02-29 08:35:45 +0530

    Delete Day 6/ex_1 directory

--- Day 6/ex_1/src/main/java/day_6_ce/ex_1/repository/ChildrenRepo.java
+++
@@ -1,9 +0,0 @@
-package day_6_ce.ex_1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import day_6_ce.ex_1.model.Children;
-
-public interface ChildrenRepo extends JpaRepository<Children , Integer>{
-    
-}
